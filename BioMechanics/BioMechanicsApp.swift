1748b92e628c1af4d8f6338757e92423c56d39d8
eNorthug <even.northug.gmail.com>
2022-01-15 20:30:47 +0100

    Initial Commit

---
+++ BioMechanics/BioMechanicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioMechanicsApp.swift
+//  BioMechanics
+//
+//  Created by Even Northug on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BioMechanicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BioMechanics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BioMechanics
+//
+//  Created by Even Northug on 15/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
